d566081801425aea0d892049eae416b866b8f461
namanjain7 <namanjain1568@gmail.com>
2021-05-01 19:17:25 +0530

    my card basic project

---
+++ flutterApps/naman_card/android/app/src/main/kotlin/com/namanjain7/naman_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.namanjain7.naman_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutterApps/naman_card/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Naman());
+}
+
+class Naman extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orange,
+        body: Container(),
+      ),
+    );
+  }
+}
